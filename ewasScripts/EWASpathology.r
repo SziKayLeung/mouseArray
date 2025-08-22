071fd31aac7c7513449aff14513ce98b614300fc
sl693 <sl693@exeter.ac.uk>
2025-08-22 18:16:19 +0100

    include cell proportion in the pathology analysis results - using CETYGO proportions from using mouse FANS and human BDR FANS data as covariates (with interaction term) - include the actual command to run EWASpathologyJobSubmission - log output

--- ewasScripts/EWASpathology.r
+++ ewasScripts/EWASpathology.r
@@ -7,9 +7,12 @@
 ##                    This scripted is adapted from one co-authored
 ##                    by EJH and EMW for the MRC schizophrenia project
 ##
-##                   
+## Adapted by SKL to also run bulk methylation                
 ##
 ##---------------------------------------------------------------------#
+
+message("Script processed: ", format(Sys.time(),'%A, %B %d, %Y %H:%M:%S'),".")
+
 
 #----------------------------------------------------------------------#
 # NOTES
@@ -29,9 +32,9 @@
 # LOAD PACKAGES
 #----------------------------------------------------------------------#
 
-library(lme4)
-library(lmerTest)
-library(dplyr)
+suppressMessages(library(lme4))
+suppressMessages(library(lmerTest))
+suppressMessages(library(dplyr))
 
 
 #----------------------------------------------------------------------#
@@ -41,25 +44,70 @@ library(dplyr)
 # if cell type = neun+ source functions without random terms
 # if cell type = neun- source functions w/ random terms
 
-runEWAS<-function(row,QCmetrics){
+runEWAS<-function(row, QCmetrics, deconvolution, species){
+
+  if(isTRUE(deconvolution)){
   
-  nullLM<-lm(row ~ QCmetrics$Sex)
+    if(species == "mouse"){
+    
+       nullLM<-lm(row ~ QCmetrics$Sex + QCmetrics$DN + QCmetrics$NEUN)
+
+       fullLM<-lm(row ~ QCmetrics$Pathology + QCmetrics$Sex + QCmetrics$DN + QCmetrics$NEUN + QCmetrics$Pathology*QCmetrics$DN)
+       
+      # extract case control main effect and sex effect
+      return(c(summary(fullLM)$coefficients["QCmetrics$Pathology",c(1,2,4)],
+               summary(fullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
+               
+               # extract sex effect
+               summary(nullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
+               
+               # extract interaction terms
+               summary(fullLM)$coefficients["QCmetrics$Pathology:QCmetrics$DN",c(1,2,4)],
+               
+               # run anova
+               anova(fullLM, nullLM)[2,"Pr(>F)"])) 
+    }else{
+    
+      nullLM<-lm(row ~ QCmetrics$Sex + QCmetrics$IRF8 + QCmetrics$NEUN + QCmetrics$SOX10 + QCmetrics$TN)
+
+      fullLM<-lm(row ~ QCmetrics$Pathology + QCmetrics$Sex + QCmetrics$IRF8 + QCmetrics$NEUN + QCmetrics$SOX10 + QCmetrics$TN + QCmetrics$Pathology*QCmetrics$IRF8 + QCmetrics$Pathology*QCmetrics$SOX10)
+       
+      # extract case control main effect and sex effect
+      return(c(summary(fullLM)$coefficients["QCmetrics$Pathology",c(1,2,4)],
+               summary(fullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
+               
+               # extract sex effect
+               summary(nullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
+               
+               # extract interaction terms
+               summary(fullLM)$coefficients["QCmetrics$Pathology:QCmetrics$IRF8",c(1,2,4)],
+               summary(fullLM)$coefficients["QCmetrics$Pathology:QCmetrics$SOX10",c(1,2,4)],
+               
+               # run anova
+               anova(fullLM, nullLM)[2,"Pr(>F)"]))
   
-  fullLM<-lm(row ~ QCmetrics$Pathology + QCmetrics$Sex)
+    }
   
   
-  # extract case control main effect and sex effect
-  return(c(summary(fullLM)$coefficients["QCmetrics$Pathology",c(1,2,4)],
-           summary(fullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
-           
-           # extract sex effect
-           summary(nullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
-           
-           # run anova
-           anova(fullLM, nullLM)[2,"Pr(>F)"]))
+  }else{
+    
+    nullLM<-lm(row ~ QCmetrics$Sex)
+    
+    fullLM<-lm(row ~ QCmetrics$Pathology + QCmetrics$Sex)
+    
+    # extract case control main effect and sex effect
+    return(c(summary(fullLM)$coefficients["QCmetrics$Pathology",c(1,2,4)],
+             summary(fullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
+             
+             # extract sex effect
+             summary(nullLM)$coefficients["QCmetrics$SexM",c(1,2,4)],
+             
+             # run anova
+             anova(fullLM, nullLM)[2,"Pr(>F)"]))
+  
+  }             
   
 }
-
 
 
 #----------------------------------------------------------------------#
@@ -78,6 +126,27 @@ projDir <- args[1]
 cellType <- args[2]
 #cellType <- "NEUNpos"
 
+# Check if third argument exists and is non-empty (i.e. whether to run pathology with CETYGO score as covariate, in which case input = CEYTGO input)
+if (length(args) < 3 || args[3] == "" || is.na(args[3])) {
+  deconvolution_input <- NULL
+  deconvolution = FALSE
+  species=NULL
+  message("No deconvolution input provided.")
+} else {
+  deconvolution_input <- args[3]
+  deconvolution = TRUE
+  message("Deconvolution input: ", deconvolution_input)
+  
+  if (length(args) < 4 || args[4] == "" || is.na(args[4])) {
+    message("Need 4th argument about species")
+  }else{
+    species <- args[4]
+  }
+  
+}
+
+
+
 # Added matching pathology column (not previously in the normalisedData)
 normData<-file.path(projDir, "2_normalised/normalisedDataPathology.rdat")
 
@@ -86,6 +155,7 @@ normData<-file.path(projDir, "2_normalised/normalisedDataPathology.rdat")
 #----------------------------------------------------------------------#
 
 setwd(projDir)
+message("Loading normalised data and QC metrics: normData")
 load(normData)
 
 
@@ -96,18 +166,18 @@ if ("Group" %in% colnames(QCmetrics)) {
 } else if ("Phenotype" %in% colnames(QCmetrics)) {
   # If 'Group' doesn't exist but 'Phenotype' exists, create 'Group' from 'Phenotype'
   QCmetrics$Group <- as.factor(QCmetrics$Phenotype)
-  print("Using 'Phenotype' as 'Group'")
+  message("Using 'Phenotype' as 'Group'")
 } else if ("Genotype" %in% colnames(QCmetrics)) {
   # If 'Group' and 'Phenotype' don't exist, but 'Genotype' exists, create 'Group' from 'Genotype'
   QCmetrics$Group <- as.factor(QCmetrics$Genotype)
-  print("Using 'Genotype' as 'Group'")
+  message("Using 'Genotype' as 'Group'")
 } else {
   # If neither 'Group', 'Phenotype', nor 'Genotype' exist
-  print("No valid group column found")
+  message("No valid group column found")
 }
 
 # subset on cell type if 2nd argument provided
-if(!is.na(cellType) && !is.null(cellType)){
+if(cellType != "bulk"){
   print(paste0("running EWAS on ", cellType, " cell type..."))
   ## subset to cell type samples
   QCmetrics<-QCmetrics[which(QCmetrics$Cell_Type == cellType),]
@@ -115,9 +185,8 @@ if(!is.na(cellType) && !is.null(cellType)){
   # subset beta matrix to cell type specific samples
   celltypeNormbeta<-celltypeNormbeta[,QCmetrics$Basename]
 }else{
-  print("running EWAS on bulk")
+  message("Running EWAS on bulk")
   celltypeNormbeta<-normBeta
-  cellType <- "bulk"
 } 
 
 # use covariate chip position if bulk tissue 
@@ -143,6 +212,33 @@ if(isFALSE(identical(colnames(celltypeNormbeta), QCmetrics$Basename))){
   QCmetrics <- QCmetrics[match(colnames(celltypeNormbeta), QCmetrics$Basename),]
 }
 
+# input CETYGO input if using it as covariate
+if(!is.null(deconvolution_input)){
+
+  message("Reading in deconvolution input file")
+  deconvolution_input <- get(load(deconvolution_input))
+  
+  # check that there is a CETYGO column
+  if(isFALSE("CETYGO" %in% colnames(deconvolution_input))){
+    print("CETGO score needs to be in the input deconvolution file")
+  }
+  
+  # check same samples are in both the QCmetric and in the CETYGO output
+  if(length(setdiff(row.names(deconvolution_input), row.names(QCmetrics))) != 0){
+    print(setdiff(row.names(deconvolution_input), row.names(QCmetrics)))
+    print(row.names(QCmetrics))
+    print("Samples in the deconvolution file that are not in the QC metric, please check and re-run")
+  }
+  
+  if(length(setdiff(row.names(QCmetrics), row.names(deconvolution_input))) != 0){
+    print("Samples in the QC metric that are not in the deconvolution input, please check and re-run")
+  }
+
+  # merge by row.names
+  QCmetrics <- merge(QCmetrics, deconvolution_input, all = T, by = 0)
+}
+
+
 # take top 100 rows for debugging
 #betasSub <- celltypeNormbeta[1:100,]
 #row <- colMeans(celltypeNormbeta)
@@ -156,42 +252,33 @@ cl <- makeCluster(nCores-1)
 registerDoParallel(cl)
 clusterExport(cl, list("runEWAS"))
 
-outtab<-matrix(data = parRapply(cl, celltypeNormbeta, runEWAS, QCmetrics), ncol = 10, byrow = TRUE)
+message("Deconvolution = ", deconvolution)
+message("Deconvolution on the species dataset = ", species) 
+
+if(isTRUE(deconvolution) & species == "mouse"){
+  outtab<-matrix(data = parRapply(cl, celltypeNormbeta, runEWAS, QCmetrics, deconvolution, species), ncol = 13, byrow = TRUE)
+  outputcolumnNames=c("Pathology_coeff", "Pathology_SE", "Pathology_P","SexM_coeff", "SexM_SE", "SexM_P", "nullSexM_coeff", "nullSexM_SE", "nullSexM_P", 
+"Pathology:DN_coeff", "Pathology:DN_SE", "Pathology:DN_P", "anovoP")
+}else if(isTRUE(deconvolution) & species == "human"){
+  outtab<-matrix(data = parRapply(cl, celltypeNormbeta, runEWAS, QCmetrics, deconvolution, species), ncol = 16, byrow = TRUE)
+  outputcolumnNames=c("Pathology_coeff", "Pathology_SE", "Pathology_P","SexM_coeff", "SexM_SE", "SexM_P", "nullSexM_coeff", "nullSexM_SE", "nullSexM_P", 
+"Pathology:IRF8_coeff", "Pathology:IRF8_SE", "Pathology:IRF8_P", "Pathology:SOX10_coeff", "Pathology:SOX10_SE", "Pathology:SOX10_P", "anovoP")
+}else{
+ outtab<-matrix(data = parRapply(cl, celltypeNormbeta, runEWAS, QCmetrics, deconvolution, species), ncol = 10, byrow = TRUE)
+ outputcolumnNames=c("Pathology_coeff", "Pathology_SE", "Pathology_P","SexM_coeff", "SexM_SE", "SexM_P", "nullSexM_coeff", "nullSexM_SE", "nullSexM_P", "anovoP")
+}
 
 
 rownames(outtab)<-rownames(celltypeNormbeta)
-#rownames(outtab)<-rownames(betasSub)
-colnames(outtab)<-c("Pathology_coeff", "Pathology_SE", "Pathology_P", 
-                    "SexM_coeff", "SexM_SE", "SexM_P",
-                
-                    "nullSexM_coeff", "nullSexM_SE", "nullSexM_P",
-                    
-                    "anovoP") 
+colnames(outtab)<-outputcolumnNames
 
-filePath <- paste0(projDir, "/3_analysis/results/", cellType, "EWASpathout.rdat")
-save(outtab, file = filePath)
-
-
-
-#----------------------------------------------------------------------#
-# Explore results
-#----------------------------------------------------------------------#
-
-
-bonfP <- 0.05/nrow(outtab)
-
-# for each col, how many resultsa are lower than bonfP
-
-countSig <- function(x){
-  sig <- sum(x < bonfP)
-  return(sig)
+if(isTRUE(deconvolution)){
+  filePath <- paste0(projDir, "/3_analysis/results/", cellType, "EWASpathout_", species, "cellTypeDeconvolution.rdat")
+  save(outtab, file = filePath)
+}else{
+  filePath <- paste0(projDir, "/3_analysis/results/", cellType, "EWASpathout.rdat")
+  save(outtab, file = filePath)
 }
 
-apply(outtab[,1:10], 2, countSig) 
-
-
-# top 20
-
-outtab <- as.data.frame(outtab)
-outtab[order(outtab$Pathology_P),][1:20,]
-
+message("Output:", filePath)
+message("EWAS finished!")
--- ewasScripts/EWASpathologyJobSubmission.sh
+++ ewasScripts/EWASpathologyJobSubmission.sh
@@ -6,39 +6,24 @@
 #SBATCH --nodes=1 #specify number of nodes
 #SBATCH --ntasks-per-node=16 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion
-#SBATCH --error=EWASpath.err # error file
-#SBATCH --output=EWASpath.log # output file
-#SBATCH --job-name=EWASpath
 
-
-#------------------------------------------------------
-
-# 1. command line argument input is:
-
-#   1. <filepath/to/projectFolder>
-#   2. cellType
-# e.g. to run sbatch EWASjobSubmission.sh <filepath/to/projectFolder> "NEUNpos"
-
-#-----------------------------------------------------
-
-## print start date and time
-echo Job started on:
-date -u
-JOBNAME="EWAS"
-
-echo Job sumbitted from:
-echo $SLURM_SUBMIT_DIR
-
-# Move the user to the project directory
-cd $1
-
+# run on command line
 
 ## load modules
 module load Pandoc
 module load R/4.2.1-foss-2022a
 
-Rscript /lustre/projects/Research_Project-MRC148213/lsl693/scripts/mouseArray/ewasScripts/EWASpathology.r $1 $2
+## input paths
+projDir=/lustre/projects/Research_Project-MRC190311/DNAm/dementiaMouse/APPKI_bulk
+scriptPath=/lustre/projects/Research_Project-MRC148213/lsl693/scripts/mouseArray/ewasScripts/EWASpathology.r
+mouseDeconvolution=/lustre/projects/Research_Project-MRC190311/DNAm/dementiaMouse/APPKI_bulk/3_analysis/CETYGO/bulk_CETYGO_predProp.RData
+humanDeconvolution=/lustre/projects/Research_Project-MRC190311/DNAm/dementiaMouse/APPKI_bulk/3_analysis/CETYGO/mouse_human_CETYGO_predProp.RData
 
-## print finish date and time
-echo Job finished on:
-date -u
+# run EWAS without CETYGO covariate 
+Rscript ${scriptPath} ${projDir} bulk &> EWASpath.log
+
+# run EWAS with CETYGO score as covariate (mouse FANS as training data, mouse bulk as testing data)
+Rscript ${scriptPath} ${projDir} bulk ${mouseDeconvolution} mouse &> ${projDir}/3_analysis/results/EWASpathMouseCetygo.log
+
+# run EWAS with CETYGO score as covariate (human FANS as training data, mouse bulk as testing data)
+Rscript ${scriptPath} ${projDir} bulk ${humanDeconvolution} human &> ${projDir}/3_analysis/results/EWASpathHumanCetygo.log
